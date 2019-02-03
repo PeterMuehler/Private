4bb3030f2ae80bc40c6906370665783c152ae3d8
PeterMuehler <john.doe.business@gmx.de>
2019-02-03 01:57:43 +0100

    Update pi-fist_steps.sh

--- pi-fist_steps.sh
+++ pi-fist_steps.sh
@@ -2,24 +2,24 @@
 
 
 #update before to update the pi
-apt update
-apt dist-upgrade -y
+sudo apt update
+sudo apt dist-upgrade -y
 
 ---
 
 #install basic programs
 
 #Install daemon to run programs in backround
-apt install daemon -y
+sudo apt install daemon -y
 #Install MidnightComander File Manager
-apt install mc -y
+sudo apt install mc -y
 #Install advanced taskmanager
-apt install htop -y
+sudo apt install htop -y
 #Install xrdp to vonnect with mstsc
-apt install xrdp -y
+sudo apt install xrdp -y
 
 ---
 
 #update after all programs are installed
-apt update
-apt dist-upgrade -y
+sudo apt update
+sudo apt dist-upgrade -y
